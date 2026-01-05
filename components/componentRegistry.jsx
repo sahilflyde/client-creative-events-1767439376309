3d687af4ee457ea14a9dbd603ff955db65c1110e
Blinkflo Builder <builder@blinkflo.dev>
2026-01-05 05:55:16 +0000

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,18 @@
+
+import HeroSection from "./heroSection";
+import MetricsSection from "./key-metric";
+import TargetAudienceSectionComp from "./targetAudienceSection";
+import GridVideo from "./GridVideo";
+import TransformSection from "./TransformSection";
+import Header from "./header";
+import Footer from "./footer";
+
+export const componentRegistry = {
+  "hero-section": HeroSection,
+  "metrics-section": MetricsSection,
+  "target-audience": TargetAudienceSectionComp,
+  "grid-video": GridVideo,
+  "transform-section": TransformSection,
+  "header-sec": Header,
+  "footer-section": Footer
+};
---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+
+/** AUTO-GENERATED FILE — DO NOT EDIT */
+export default {
+  reactStrictMode: true,
+
+  images: {
+    domains: ["ik.imagekit.io","placehold.co"],
+  },
+
+  env: {
+    SITE_SLUG: "creative-events-1767439376309",
+  },
+};
